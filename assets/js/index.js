8ad9ca35a3237f6c34f6c734c37b553a6bb15f66
mojib2014 <mojib_monir@yahoo.com>
2021-09-10 02:14:00 -0700

    Add: functionality for slideshows , ome stylingg, and add mobile menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,52 @@
 // DOM Elements
 const header = document.getElementById("header");
+const topnav = document.querySelector(".topnav");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuIcon = document.querySelector(".menu");
 const banner = document.getElementById("banner");
 const carouselImg = document.getElementById("carousel-img");
 const images = document.querySelectorAll(".carousel-img");
 const playBtns = document.querySelectorAll("#video-play-btn");
-const previousBtn = document.querySelector(".zippy-life-images .previous");
-const next = document.querySelector(".next");
+const videoSlides = document.querySelectorAll(
+  ".video-projects-flex-container .flex-item",
+);
+const dots = document.querySelectorAll(".dot");
+const previousBtns = document.querySelectorAll(".previous");
+const nextBtns = document.querySelectorAll(".next");
+
+// Mobile menu
+menuIcon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (mobileMenu.style.marginLeft === "-40%") {
+    menuIcon.classList.add("close");
+    mobileMenu.style.marginLeft = "0";
+  } else {
+    menuIcon.classList.remove("close");
+    mobileMenu.style.marginLeft = "-40%";
+  }
+}
+
+// By clicking on header's anchor tags close mobile menu
+activeClass(topnav);
+activeClass(mobileMenu);
+function activeClass(container) {
+  // Loop through the buttons and add the active class to the current/clicked button
+  const items = container.querySelectorAll("a");
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function () {
+      let current = container.querySelectorAll(".active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+}
+
 // Navbar active class
 
 // Slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
 
 function currentSlide(n) {
   showSlides((slideIndex = n));
@@ -23,7 +55,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("slides");
-  const dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -40,15 +71,26 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+dots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    const dataIndex = e.target.getAttribute("data-index");
+    currentSlide(dataIndex);
+  });
+});
+
 // Change header color on scroll
 const sticky = header.offsetTop;
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", scroll);
+
+function scroll() {
   // minus 70 cause it needs to start before it passes banner section
   if (window.pageYOffset > banner.clientHeight - 70)
     header.classList.add("scrolled");
   else header.classList.remove("scrolled");
-});
+}
+
+if (window.innerWidth < 900) window.removeEventListener("scroll", scroll);
 
 // Life in Zippy carousel
 images.forEach((img) => {
@@ -58,6 +100,7 @@ images.forEach((img) => {
   });
 });
 
+// Videos Projects Section
 // Play and Pause video
 playBtns.forEach((playBtn) => {
   playBtn.addEventListener("click", (e) => {
@@ -75,6 +118,55 @@ playBtns.forEach((playBtn) => {
     }
   });
 });
+
+// Videos slideshow
+let videoSlideIndex = 1;
+
+showVideoSlides(videoSlideIndex);
+
+function plusSlides(n) {
+  showVideoSlides((videoSlideIndex += n));
+}
+
+function showVideoSlides(n) {
+  if (n > videoSlides.length) {
+    videoSlideIndex = 1;
+  }
+  if (n < 1) {
+    videoSlideIndex = videoSlides.length;
+  }
+
+  for (let i = 0; i < videoSlides.length; i++) {
+    videoSlides[i].style.display = "none";
+  }
+
+  videoSlides[videoSlideIndex - 1].style.display = "block";
+  videoSlides[videoSlideIndex].style.display = "block";
+  videoSlides[videoSlideIndex + 1].style.display = "block";
+}
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    plusSlides(1);
+    videoSlides.forEach((slide) => {
+      slide.classList.add("slide-in-from-right");
+      slide.classList.remove("slide-in-from-left");
+    });
+  });
+});
+
+previousBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    plusSlides(-1);
+
+    videoSlides.forEach((slide) => {
+      slide.classList.add("slide-in-from-left");
+      slide.classList.remove("slide-in-from-right");
+    });
+  });
+});
+
+// Life in Zippy slideshow
 
 // Initialize google map
 let map;
